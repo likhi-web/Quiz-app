4f9a2f557b64fe6c6f94aa2a67e0f6bcfd3ddab9
SRK <uilikitha@gmail.com>
2025-05-22 20:54:21 +0530

    quizz app

---
+++ src/assets/data.js
@@ -0,0 +1,51 @@
+
+
+export const data = [
+    {
+       
+        question: "What is the capital of France?",
+        option1: "Berlin",
+        option2: "Madrid",
+        option3: "Paris",
+        option4: "Rome",
+        ans: 3
+    },
+    {
+       
+        question: "Which planet is known as the Red Planet?",
+        option1: "Earth",
+        option2: "Mars",
+        option3: "Jupiter",
+        option4: "Saturn",
+        ans: 2
+    },
+    {
+        
+        question: "Who wrote 'Romeo and Juliet'?",
+        option1: "Charles Dickens",
+        option2: "William Shakespeare",
+        option3: "Mark Twain",
+        option4: "Jane Austen",
+        ans: 2
+    },
+    {
+       
+        question: "What is the largest ocean on Earth?",
+        option1: "Atlantic Ocean",
+        option2: "Indian Ocean",
+        option3: "Arctic Ocean",
+        option4: "Pacific Ocean",
+        ans: 4
+    },
+    
+    {
+       
+        question: "What is the smallest prime number?",
+        option1: "0",
+        option2: "1",
+        option3: "2",
+        option4: "3",
+        ans: 3
+    },
+   
+]
